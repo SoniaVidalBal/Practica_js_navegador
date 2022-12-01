c7ea3dc73056bf93ee250ab2b0d38c68eb91fd02
Sonia Vidal <soniaavidaal@gmail.com>
2022-12-01 21:04:06 +0100

    Proba 3. Falta solucionar sumes

--- index_js.js
+++ index_js.js
@@ -1,18 +1,20 @@
 
 const button = document.querySelector(".button");
+const inputconcept = document.querySelector("#concept")
+const inputquantity = document.querySelector("#quantity")
 
 button.addEventListener("click", () => {
-    const inputconcept = document.querySelector("#concept")
-    const inputquantity = document.querySelector("#quantity")
     localStorage.setItem("concepto", inputconcept.value)
     localStorage.setItem("cantidad", inputquantity.value)
     const child = document.createElement('li')
     child.textContent = `${inputconcept.value} ${inputquantity.value}`
     const elementList = document.querySelector(".transactions")
     elementList.appendChild(child)
+    incomesexpenses()
     inputconcept.value = '';
     inputquantity.value = '';
 })
+
 const input1localstorage = localStorage.getItem("concepto")
 const input2localstorage = localStorage.getItem("cantidad")
 if (input1localstorage && input2localstorage) {
@@ -21,3 +23,22 @@ if (input1localstorage && input2localstorage) {
     child.textContent = `${input1localstorage} ${input2localstorage}`
     elementList.appendChild(child)
 }
+
+function incomesexpenses(){
+    //const gastos = document.querySelector(".expense")
+    const ingresos = document.querySelector(".income")
+    const gastos = document.querySelector(".expense")
+    const nums = parseInt(inputquantity.value)
+        if (nums < 0){
+            localStorage.setItem("cantidadnegativa", nums)
+            const inputnegative = localStorage.getItem("cantidadnegativa")
+            const nums2 = (parseInt(inputnegative) + nums)
+            return gastos.textContent = nums2}
+        else if(nums > 0){
+            localStorage.setItem("cantidadpositiva", nums)
+            const inputpositive = localStorage.getItem("cantidadpositiva")
+            const nums3 = (parseInt(inputpositive) + nums)
+            return ingresos.textContent = nums3
+        }
+}
+
