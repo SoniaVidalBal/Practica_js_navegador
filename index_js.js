b13bd1a505422718e04d74656cdf17d33a4671fb
Sonia Vidal <soniaavidaal@gmail.com>
2022-12-08 18:00:04 +0100

    Acabado. No funciona del todo bien local storage.

--- index_js.js
+++ index_js.js
@@ -2,13 +2,14 @@
 const create_transaction = document.querySelector(".addtransaction");
 const inputconcept = document.querySelector("#concept");
 const inputquantity = document.querySelector("#quantity");
+let transactionsStorage = [];
 
 create_transaction.addEventListener("submit", (event) => {
     event.preventDefault();
-    localStorage.setItem("transaccion", `${inputconcept.value}: ${inputquantity.value}`);
     gettransaction();
     incomes_expenses(inputquantity.value);
     total();
+    localStorage.setItem("transaccion", JSON.stringify(transactionsStorage));
     inputconcept.value = '';
     inputquantity.value = '';
 })
@@ -19,77 +20,97 @@ function gettransaction(){
         quantity: inputquantity.value
     };
 
-    const child = document.createElement('li')
-    child.textContent = `${transaction.concept}: ${transaction.quantity}`
-    const elementList = document.querySelector(".transactions")
-    elementList.prepend(child)
-    
-    const close = document.createElement('button')
-    close.textContent = 'x'
-    child.appendChild(close)
-    close.addEventListener('click', () => {
-        elementList.removeChild(child)
-        delete_transaction(transaction.quantity)
-    })
-    
+    const child = document.createElement('li');
+    child.textContent = `${transaction.concept}: ${transaction.quantity}`;
+    const elementList = document.querySelector(".transactions");
+    elementList.prepend(child);
 
+    const close = document.createElement('button');
+    close.textContent = 'x';
+    child.appendChild(close);
+    close.addEventListener('click', () => {
+        elementList.removeChild(child);
+        delete_transaction(transaction.quantity);
+    });
+
+    transactionsStorage.push(transaction);
 }
 
-let positive_operations = []
-let negative_operations = []
-const ingresos = document.querySelector(".income")
-const gastos = document.querySelector(".expense")
-const displaysumatotal = document.querySelector(".total")
+let positive_operations = [];
+let negative_operations = [];
+const ingresos = document.querySelector(".income");
+const gastos = document.querySelector(".expense");
+const displaysumatotal = document.querySelector(".total");
 
 function incomes_expenses(inputquantity){
-    const nums = parseFloat(inputquantity)
-        if (nums < 0){
-            negative_operations.push(nums)
-            gastos.textContent = negative_operations.reduce((accum, operation) => accum + operation, 0)
-            localStorage.setItem("gastos", gastos.textContent)
-            return (gastos.textContent)
-        }else if(nums > 0){
-            positive_operations.push(nums)
-            ingresos.textContent = positive_operations.reduce((accum, operation) => accum + operation, 0)
-            localStorage.setItem("ingresos", ingresos.textContent)
-            return (ingresos.textContent)
-        }
+    const nums = parseFloat(inputquantity);
+    if (nums < 0){
+        negative_operations.push(nums);
+        gastos.textContent = negative_operations.reduce((accum, operation) => accum + operation, 0);
+        localStorage.setItem("gastos", gastos.textContent);
+        return (gastos.textContent);
+    }else if(nums > 0){
+        positive_operations.push(nums);
+        ingresos.textContent = positive_operations.reduce((accum, operation) => accum + operation, 0);
+        localStorage.setItem("ingresos", ingresos.textContent);
+        return (ingresos.textContent);
+    }
 }
 
 function total(){
-
-    const totalingresos = positive_operations.reduce((accum, operation) => accum + operation, 0)
-    const totalgastos = negative_operations.reduce((accum, operation) => accum + operation, 0)
-    const sumatotal = totalingresos + totalgastos
-    localStorage.setItem("total", sumatotal)
-    return displaysumatotal.textContent = sumatotal
+    const totalingresos = positive_operations.reduce((accum, operation) => accum + operation, 0);
+    const totalgastos = negative_operations.reduce((accum, operation) => accum + operation, 0);
+    const sumatotal = totalingresos + totalgastos;
+    localStorage.setItem("total", sumatotal);
+    return displaysumatotal.textContent = sumatotal;
 }
 
 function delete_transaction(transactionquant){
-    positive_operations = positive_operations.filter((item) => item != transactionquant)
-    negative_operations = negative_operations.filter((item) => item != transactionquant)
-    ingresos.textContent = positive_operations.reduce((accum, operation) => accum + operation, 0)
-    gastos.textContent = negative_operations.reduce((accum, operation) => accum + operation, 0)
-    total()
+    positive_operations = positive_operations.filter((item) => item != transactionquant);
+    negative_operations = negative_operations.filter((item) => item != transactionquant);
+    ingresos.textContent = positive_operations.reduce((accum, operation) => accum + operation, 0);
+    gastos.textContent = negative_operations.reduce((accum, operation) => accum + operation, 0);
+    total();
 }
 
-const input1localstorage = localStorage.getItem("transaccion")
-const incomeStorage = localStorage.getItem("ingresos")
-const expenseStorage = localStorage.getItem("gastos")
-const totalStorage = localStorage.getItem("total")
-if (input1localstorage) {
-    const child = document.createElement('li')
-    const elementList = document.querySelector(".transactions")
-    child.textContent = input1localstorage
-    elementList.appendChild(child)
+let inputLocalStorage = JSON.parse(localStorage.getItem('transaccion'));
+const incomeStorage = localStorage.getItem("ingresos");
+const expenseStorage = localStorage.getItem("gastos");
+const totalStorage = localStorage.getItem("total");
+
+if (inputLocalStorage) {
+    for (let index = 0; index < inputLocalStorage.length; index++) {
+        const child = document.createElement('li');
+        child.textContent = `${inputLocalStorage[index].concept}: ${inputLocalStorage[index].quantity}`;
+        const elementList = document.querySelector(".transactions");
+        elementList.prepend(child);
+
+        const close = document.createElement('button');
+        close.textContent = 'x';
+        child.appendChild(close);
+        close.addEventListener('click', () => {
+            elementList.removeChild(child);
+            displaysumatotal.textContent = (parseFloat(totalStorage) - (parseFloat(inputLocalStorage[index].quantity)));
+            if (parseFloat(inputLocalStorage[index].quantity) > 0){
+                const newIncomes = parseFloat(incomeStorage)-parseFloat(inputLocalStorage[index].quantity)
+                ingresos.textContent = newIncomes
+            } else if (parseFloat(inputLocalStorage[index].quantity) < 0){
+                const newExpeses = parseFloat(expenseStorage)-parseFloat(inputLocalStorage[index].quantity)
+                gastos.textContent = newExpeses
+            }
+        }); 
+    }
 }
+
 if (incomeStorage){
-    incomes_expenses(incomeStorage)
+    incomes_expenses(incomeStorage);
 }
+
 if (expenseStorage){
-    incomes_expenses(expenseStorage)
+    incomes_expenses(expenseStorage);
 }
+
 if (totalStorage){
-    displaysumatotal.textContent = totalStorage
+    displaysumatotal.textContent = totalStorage;
 }
 
